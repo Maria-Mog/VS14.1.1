9652068539d2641ddb34e67d2c3cb6c5a6b44530
Мария <1481047@mail.ru>
2021-12-03 14:38:46 +0300

    Добавьте файлы проекта.

---
+++ VS14.1.1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS14._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Муся");
+            cat.ShowInfo();
+            Dog dog = new Dog("Терри");
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+    abstract class Animal
+    {        
+        public abstract string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+        public virtual void ShowInfo()
+
+        {
+            Console.Write("{0} ", Name);
+            Say();
+        }
+    }
+    class Cat : Animal
+    {
+        string name;
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public Cat(string name)
+            : base(name)
+        {
+
+        }
+        public override void Say()
+        {
+            Console.WriteLine("\"Мяу\"");
+        }
+        
+    }
+    class Dog : Animal
+    {
+        string name;
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public Dog(string name)
+            : base(name)
+        {
+
+        }
+        public override void Say()
+        {
+            Console.WriteLine("\"Гав\"");
+        }
+
+    }
+
+}
